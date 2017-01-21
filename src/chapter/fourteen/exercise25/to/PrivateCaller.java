2106317a7b3545394ec9da26fab288b72bb19ebd
yknnnnft <neril.lee@gmail.com>
2017-01-21 23:39:46 +0900

    exercise on private field

--- src/chapter/fourteen/exercise25/to/PrivateCaller.java
+++ src/chapter/fourteen/exercise25/to/PrivateCaller.java
@@ -1,5 +1,6 @@
 package chapter.fourteen.exercise25.to;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -10,11 +11,16 @@ public class PrivateCaller {
 	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		// TODO Auto-generated method stub
 		Class<?> c = ClassHasPrivate.class;
-
+		ClassHasPrivate cp = new ClassHasPrivate();
+		Field[] allFields = c.getDeclaredFields();
+		for (Field f : allFields) {
+			f.setAccessible(true); // IllegalAccessException without this code
+			System.out.println(f.get(cp));
+		}
 		Method[] allMethods = c.getDeclaredMethods();
 		for (Method m : allMethods) {
 			m.setAccessible(true);
-			m.invoke(new ClassHasPrivate());
+			m.invoke(cp);
 		}
 		
 	}
